d1c7f7ab5d91dc32588ada3f7e760b9bfba0c4cc
gilpgform <gilpgform@gmail.com>
2021-01-26 00:49:41 -0600

    inicia 16_bd

---
+++ 16_bd/cmp/nav-local.js
@@ -0,0 +1,65 @@
+/* This work by Gilberto Pacheco Gallegos is licensed under the Creative Commons
+ * Atribución 4.0 Internacional License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by/4.0/. */
+
+customElements.define("nav-local", class extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `<ol>
+        <li><p><a href="index.html">Bases de Datos</a></p></li>
+        <li><p><a href="2_contenido.html">Contenido</a></p></li>
+        <li><p><a href="3_casos.html">Casos de Uso</a></p></li>
+        <li><p><a href="4_er.html">Diagrama Entidad Relación</a></p></li>
+        <li><p><a href="5_no_rel.html">Diseño no Relacional</a></p></li>
+        <li><p><a href="6_ejemplo.html">Datos de Ejemplo</a></p></li>
+        <li><p><a href="7_instrucciones.html">Instrucciones</a></p></li>
+        <li><p><a href="8_archivos.html">Archivos</a></p></li>
+        <li><p><a href="9_footer.html">cmp/mi-footer.js</a></p></li>
+        <li><p><a href="10_progreso.html">cmp/mi-progreso.js</a></p></li>
+
+        <li><p><a href="11_tipos.html">js/tipos.js</a></p></li>
+        <li><p><a href="12_util.html">lib/util.js</a></p></li>
+
+        <li><p><a href="13_index.html">index.html</a></p></li>
+        <li><p><a href="14_init.html">js/init.js</a></p></li>
+        <li><p><a href="15_CtrlSesion.html">js/CtrlSesion.js</a></p></li>
+        <li><p><a href="16_fabrica.html">lib/fabrica.js</a></p></li>
+        <li><p><a href="17_seguridad.html">js/seguridad.js</a></p></li>
+        <li><p><a href="18_nav.html">cmp/mi-nav.js</a></p></li>
+
+        <li><p><a href="19_chat.html">chat.html</a></p></li>
+        <li><p><a href="20_CtrlChat.html">js/CtrlChat.js</a></p></li>
+
+        <li><p><a href="21_pasatiempos.html">pasatiempos.html</a></p></li>
+        <li><p><a href="22_CtrlPasatiempos.html">js/CtrlPasatiempos.js</a></p></li>
+
+        <li><p><a href="23_pasatiempoNuevo.html">pasatiempoNuevo.html</a></p></li>
+        <li><p><a href="24_CtrlPasatiempoNuevo.html">js/CtrlPasatiempoNuevo.js</a></p></li>
+        <li><p><a href="25_navegacion.html">js/navegacion.js</a></p></li>
+
+        <li><p><a href="26_pasatiempo.html">pasatiempo.html</a></p></li>
+        <li><p><a href="27_CtrlPasatiempo.html">js/CtrlPasatiempo.js</a></p></li>
+
+        <li><p><a href="28_usuarios.html">usuarios.html</a></p></li>
+        <li><p><a href="29_CtrlUsuarios.html">js/CtrlUsuarios.js</a></p></li>
+
+        <li><p><a href="30_usuarioNuevo.html">usuarioNuevo.html</a></p></li>
+        <li><p><a href="31_CtrlUsuarioNuevo.html">js/CtrlUsuarioNuevo.js</a></p></li>
+
+        <li><p><a href="32_usuario.html">usuario.html</a></p></li>
+        <li><p><a href="33_CtrlUsuario.html">js/CtrlUsuario.js</a></p></li>
+        <li><p><a href="34_js_usuarios.html">js/usuarios.js</a></p></li>
+        <li><p><a href="35_storage.html">lib/storage.js</a></p></li>
+
+        <li><p><a href="36_tiposFire.html">lib/tiposFire.js</a></p></li>
+
+        <li><p><a href="37_estilos.html">css/estilos.css</a></p></li>
+        <li><p><a href="38_listas.html">css/listas.css</a></p></li>
+        <li><p><a href="39_nav_css.html">css/mi-nav.css</a></p></li>
+
+        <li><p><a href="40_favicon.html">favicon.ico</a></p></li>
+        <li><p><a href="41_LICENSE.html">LICENSE</a></p></li>
+        <li><p><a href="42_README.html">README.md</a></p></li>
+      </ol>`;
+  }
+});
