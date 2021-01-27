db4f56c7361fdbb7faf44bb162b26c1214419798
gilpgform <gilpgform@gmail.com>
2021-01-26 21:21:16 -0600

    sw 1.05

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
-const CACHE = "gilpgpoojs-1.04";
+const CACHE = "gilpgpoojs-1.05";
 
 /**  Archivos requeridos para que la aplicación funcione fuera de línea. */
 const ARCHIVOS = [
