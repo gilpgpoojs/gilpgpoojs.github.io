7911c484da50a11d9223ead6bb3e3c44dcea4bd9
gilpgmisitio <gilpgmisitio@gmail.com>
2021-02-23 10:24:54 -0600

    sw 1.18

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
-const CACHE = "gilpgpoojs-1.17";
+const CACHE = "gilpgpoojs-1.18";
 
 /**  Archivos requeridos para que la aplicación funcione fuera de línea. */
 const ARCHIVOS = [
